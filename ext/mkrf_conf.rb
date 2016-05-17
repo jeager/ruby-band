f1d6e339fbd0d93956607fd54db952dfea4b9582
Vinicius Franco <viniciusf.arantes@gmail.com>
2016-05-17 15:50:47 -0300

    fix

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -55,7 +55,7 @@ RAKE
     if command?("mvn")==false
       rakefile.write <<-RAKE
   	    task :apt_install do
-          sh "sudo apt-get install maven2"
+         # sh "sudo apt-get install maven2"
         end
         task :default => [:apt_install]
       RAKE
